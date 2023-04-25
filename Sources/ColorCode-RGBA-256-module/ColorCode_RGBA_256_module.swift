1190b65284d6623911f7ba82f0f37e619517f26a
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-04-25 05:13:10 +0200

    Initial Commit

---
+++ Sources/ColorCode-RGBA-256-module/ColorCode_RGBA_256_module.swift
@@ -0,0 +1,6 @@
+public struct ColorCode_RGBA_256_module {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/ColorCode-RGBA-256-moduleTests/ColorCode_RGBA_256_moduleTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ColorCode_RGBA_256_module
+
+final class ColorCode_RGBA_256_moduleTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ColorCode_RGBA_256_module().text, "Hello, World!")
+    }
+}
